6efaf31ca31413d0092f23ff09651ce6cbd69965
wisecodeworks <taterdev@gmail.com>
2020-10-25 15:24:04 -0400

    Update functions.php
    I added 3 widget areas for the footer which will allow for those sections to appear. A future task is to make the footer either 50% or 1/3's.

--- functions.php
+++ functions.php
@@ -106,6 +106,33 @@ function bootscores_widgets_init() {
 		'before_title'  => '<h3 class="widget-title">',
 		'after_title'   => '</h3>',
 	) );
+	register_sidebar( array(
+		'name'          => esc_html__( 'footer-1', 'bootscores' ),
+		'id'            => 'footer-1',
+		'description'   => '',
+		'before_widget' => '<aside id="%1$s" class="widget %2$s">',
+		'after_widget'  => '</aside>',
+		'before_title'  => '<h3 class="widget-title">',
+		'after_title'   => '</h3>',
+	) );
+	register_sidebar( array(
+		'name'          => esc_html__( 'footer-2', 'bootscores' ),
+		'id'            => 'footer-2',
+		'description'   => '',
+		'before_widget' => '<aside id="%1$s" class="widget %2$s">',
+		'after_widget'  => '</aside>',
+		'before_title'  => '<h3 class="widget-title">',
+		'after_title'   => '</h3>',
+	) );
+	register_sidebar( array(
+		'name'          => esc_html__( 'footer-3', 'bootscores' ),
+		'id'            => 'ooter-3',
+		'description'   => '',
+		'before_widget' => '<aside id="%1$s" class="widget %2$s">',
+		'after_widget'  => '</aside>',
+		'before_title'  => '<h3 class="widget-title">',
+		'after_title'   => '</h3>',
+	) );
 }
 add_action( 'widgets_init', 'bootscores_widgets_init' );
 
